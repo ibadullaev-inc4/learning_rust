d38c16f1a48559cce63f5cc203c56f21852aea85
Nariman Ibadullayev <n.ibadullaev@inc4.net>
2022-09-12 22:50:13 +0400

    overflow

---
+++ overflow/target/release/deps/overflow-74619a38b37d79f1.d
@@ -0,0 +1,5 @@
+/home/nariman/Desktop/nestjs/learning_rust/overflow/target/release/deps/overflow-74619a38b37d79f1: src/main.rs
+
+/home/nariman/Desktop/nestjs/learning_rust/overflow/target/release/deps/overflow-74619a38b37d79f1.d: src/main.rs
+
+src/main.rs:
---
+++ overflow/target/release/overflow.d
@@ -0,0 +1,1 @@
+/home/nariman/Desktop/nestjs/learning_rust/overflow/target/release/overflow: /home/nariman/Desktop/nestjs/learning_rust/overflow/src/main.rs
