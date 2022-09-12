577af7a39cdb8d99ad38e3f3eac25a2939720791
Nariman Ibadullayev <n.ibadullaev@inc4.net>
2022-09-12 22:31:35 +0400

    cargo hello world

---
+++ hello-world/target/release/hello-world.d
@@ -0,0 +1,1 @@
+/home/nariman/Desktop/nestjs/learning_rust/hello-world/target/release/hello-world: /home/nariman/Desktop/nestjs/learning_rust/hello-world/src/main.rs
---
+++ hello-world/target/rls/debug/deps/hello_world-6f2dfe75c114f26b.d
@@ -0,0 +1,5 @@
+/home/nariman/Desktop/nestjs/learning_rust/hello-world/target/rls/debug/deps/hello_world-6f2dfe75c114f26b.rmeta: src/main.rs
+
+/home/nariman/Desktop/nestjs/learning_rust/hello-world/target/rls/debug/deps/hello_world-6f2dfe75c114f26b.d: src/main.rs
+
+src/main.rs:
